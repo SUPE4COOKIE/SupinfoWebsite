92f8ca0c57c8f4d132c8d4b5da5eb23be6b47242
supe4cookie <supe4cookie@gmail.com>
2022-10-26 22:04:05 +0200

    favicon + stats on mainpage

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const screenobserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
+        if (entry.isIntersecting) { // check if the element is in the viewport
 
-            entry.target.classList.add('animatedscreen');
+            entry.target.classList.add('animatedscreen'); // add the class to the element
         }
         else {
-            entry.target.classList.remove('animatedscreen');
+            entry.target.classList.remove('animatedscreen'); // remove the class from the element
         }
     });
 });
@@ -21,6 +21,28 @@ const historytextobserver = new IntersectionObserver((entries) => {
     });
 });
 
+const Franceobserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animatedFrance');
+        }
+        else {
+            entry.target.classList.remove('animatedFrance');
+        }
+    });
+});
+
+const iconcontainerobserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animatediconcontainer');
+        }
+        else {
+            entry.target.classList.remove('animatediconcontainer');
+        }
+    });
+});
+
 function flex() {
     document.getElementById("programmes").style.display = "flex";
 }
@@ -29,11 +51,15 @@ function none() {
     document.getElementById("programmes").style.display = "none";
 }
 
-const screen = document.querySelectorAll(".screen")
-const historytext = document.querySelectorAll(".historytext")
+const screen = document.querySelectorAll(".screen");
+const historytext = document.querySelectorAll(".historytext");
+const France = document.querySelectorAll(".France");
+const iconcontainer = document.querySelectorAll(".iconcontainer");
 screen.forEach((screen) => { screenobserver.observe(screen) })
 historytext.forEach((historytext) => { historytextobserver.observe(historytext) })
-
+France.forEach((France) => { Franceobserver.observe(France) })
+iconcontainer.forEach((iconcontainer) => { iconcontainerobserver.observe(iconcontainer) })
+/* call a function to modify the display type of the element if it's hovering it*/
 document.getElementById("programme").onmouseenter = flex;
 document.getElementById("programmes").onmouseenter = flex;
 document.getElementById("programme").onmouseleave = none;
